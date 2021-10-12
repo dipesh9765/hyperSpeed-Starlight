25e2a6584c5a34d5da645d85a53187adb04528a5
Dipesh Marathe <dipeshmarathe9765@gmail.com>
2021-10-12 22:12:47 +0530

    star wars hyperspeed star formation

---
+++ sketch.js
@@ -0,0 +1,21 @@
+ 
+let stars = [];
+
+let speed;
+
+function setup() {
+  createCanvas(600, 600);
+  for (let i = 0; i < 800; i++) {
+    stars[i] = new Star();
+  }
+}
+
+function draw() {
+  speed = map(mouseX, 0, width, 0, 50);
+  background(0);
+  translate(width / 2, height / 2);
+  for (let i = 0; i < stars.length; i++) {
+    stars[i].update();
+    stars[i].show();
+  }
+}
